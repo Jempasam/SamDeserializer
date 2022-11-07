65546ae2ca14579c9d5639a45683f046fed46f56
demont samuel <samuel.demont@etu.unice.fr>
2022-11-07 23:44:02 +0100

    some

--- src/jempasam/objectmanager/ObjectManager.java
+++ src/jempasam/objectmanager/ObjectManager.java
@@ -1,6 +1,7 @@
 package jempasam.objectmanager;
 
 import java.util.Map.Entry;
+import java.util.function.Supplier;
 
 import jempasam.samstream.Streamable;
 
@@ -9,4 +10,10 @@ public interface ObjectManager<T> extends Streamable<Entry<String,T>> {
 	String idOf(T name);
 	T register(String name, T obj);
 	int size();
+	
+	default <Y extends T> Y getOrDefault(Class<Y> type, String name, Supplier<Y> def) {
+		T ret=get(name);
+		if(ret!=null && type.isInstance(ret)) return type.cast(ret);
+		else return def.get();
+	}
 }
